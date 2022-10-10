41e9fb53ce4257ab48051c03904c948d1e4a1a6a
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-10-10 07:35:27 -0600

    Add project files.

---
+++ Skills_Assessmetnt_Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Skills_Assessmetnt_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>(6);
+
+
+            for (int i = 1; i <= 6; i++)
+            {
+                stack.Push(i);
+            }
+
+            Console.WriteLine(stack.Count);
+
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+
+            Console.WriteLine(stack.Count);
+        }
+    }
+}
